3a029e5f2d6484a1602398a2256041aab753823a
Pradeep <deeppradeep1998@gmail.com>
2024-10-16 08:52:46 +0530

    first commit

---
+++ 1.py
@@ -0,0 +1,10 @@
+Dict={1:2,3:4,5:6}
+result=Dict.pop(3,5)
+print(result)
+
+#4 - Output
+
+
+#Pops the value given within the bracket
+#If the key is not there then the default value 5 will be printed.
+
